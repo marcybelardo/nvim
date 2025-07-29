d6d410b8c59e86d388038fe680436b7b6089b7d4
Marceline Belardo <marcelinebelardo@gmail.com>
2025-07-29 09:08:29 +0300

    deleted conform, fixed some stuff with plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -52,6 +52,7 @@ require("mini.completion").setup({})
 local gen_loader = require("mini.snippets").gen_loader
 require("mini.snippets").setup({
 	snippets = {
+		gen_loader.from_file("~/.config/nvim/snippets/global.json"),
 		gen_loader.from_lang(),
 	}
 })
